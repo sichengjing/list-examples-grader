441ab9e7604ef6711c77ac1e1b4cce46af312cf6
sichengjing <sjing@ucsd.edu>
2022-11-10 10:40:13 -0800

    1

--- grade.sh
+++ grade.sh
@@ -4,4 +4,12 @@ set -e
 
 rm -rf student-submission
 git clone $1 student-submission
+if [-f "student-submission/ListExample.java"] 
+then exit
+else echo "Wrong file!"
+fi
 
+cp TestListExamples.java student-submission
+cd student-submission
+javac ListExample.java TestListExamples.java
+java ListExample TestListExamples
